a5f818dd077fe6fa970428f59c89a276248f6071
Josh <>
2023-04-21 21:30:10 +0800

    feat: [headr] finish

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Rust cli
 
 1. echor :heavy_check_mark:
-2. catr
+2. catr :heavy_check_mark:
+3. headr :heavy_check_mark:
--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -1,5 +1,7 @@
 use clap::{App, Arg};
 use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Read};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,27 +12,12 @@ pub struct Config {
     bytes: Option<usize>,
 }
 
+// --------------------------------------------------
 pub fn get_args() -> MyResult<Config> {
     let matches = App::new("headr")
         .version("0.1.0")
         .author("Josh Chou")
         .about("Rust head")
-        .arg(
-            Arg::with_name("files")
-                .value_name("FILE")
-                .help("Input file(s)")
-                .multiple(true)
-                .default_value("-"),
-        )
-        .arg(
-            Arg::with_name("bytes")
-                .value_name("BYTES")
-                .short("c")
-                .long("bytes")
-                .help("Number of bytes")
-                .takes_value(true)
-                .conflicts_with("lines"),
-        )
         .arg(
             Arg::with_name("lines")
                 .short("n")
@@ -38,6 +25,22 @@ pub fn get_args() -> MyResult<Config> {
                 .value_name("LINES")
                 .help("Number of lines")
                 .default_value("10"),
+        )
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .value_name("BYTES")
+                .takes_value(true)
+                .conflicts_with("lines")
+                .help("Number of bytes"),
+        )
+        .arg(
+            Arg::with_name("files")
+                .value_name("FILE")
+                .help("Input file(s)")
+                .multiple(true)
+                .default_value("-"),
         )
         .get_matches();
 
@@ -60,11 +63,53 @@ pub fn get_args() -> MyResult<Config> {
     })
 }
 
+// --------------------------------------------------
 pub fn run(config: Config) -> MyResult<()> {
-    println!("{:#?}", config);
+    let num_files = config.files.len();
+
+    for (file_num, filename) in config.files.iter().enumerate() {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(mut file) => {
+                if num_files > 1 {
+                    println!(
+                        "{}==> {} <==",
+                        if file_num > 0 { "\n" } else { "" },
+                        &filename
+                    );
+                }
+
+                if let Some(num_bytes) = config.bytes {
+                    let mut handle = file.take(num_bytes as u64);
+                    let mut buffer = vec![0; num_bytes];
+                    let bytes_read = handle.read(&mut buffer)?;
+                    print!("{}", String::from_utf8_lossy(&buffer[..bytes_read]));
+                } else {
+                    let mut line = String::new();
+                    for _ in 0..config.lines {
+                        let bytes = file.read_line(&mut line)?;
+                        if bytes == 0 {
+                            break;
+                        }
+                        print!("{}", line);
+                        line.clear();
+                    }
+                }
+            }
+        }
+    }
     Ok(())
 }
 
+// --------------------------------------------------
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
+}
+
+// --------------------------------------------------
 fn parse_positive_int(val: &str) -> MyResult<usize> {
     match val.parse() {
         Ok(n) if n > 0 => Ok(n),
@@ -72,16 +117,20 @@ fn parse_positive_int(val: &str) -> MyResult<usize> {
     }
 }
 
+// --------------------------------------------------
 #[test]
 fn test_parse_positive_int() {
+    // 3 is an OK integer
     let res = parse_positive_int("3");
     assert!(res.is_ok());
     assert_eq!(res.unwrap(), 3);
 
+    // Any string is an error
     let res = parse_positive_int("foo");
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "foo".to_string());
 
+    // A zero is an error
     let res = parse_positive_int("0");
     assert!(res.is_err());
     assert_eq!(res.unwrap_err().to_string(), "0".to_string());
